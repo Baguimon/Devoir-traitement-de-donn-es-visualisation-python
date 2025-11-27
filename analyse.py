0f47f863efab6ca4f8405b4877145d5bdd73c544
Mathis Truong <mathis.truong@next-u.fr>
2025-11-27 16:07:01 +0100

    lecture fichier

--- analyse.py
+++ analyse.py
@@ -1,3 +1,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+df_raw = pd.read_csv("result.csv")
+print(df_raw.head())
+print(df_raw.info())
+print(df_raw.memory_usage(deep=True).sum() / 1024, "Ko")
